b5ecb630c84e8d4d17897f44e47d586aa00fb450
tranmanhtuong123 <18130275@st.hcmuaf.edu.vn>
2022-04-06 21:15:14 +0700

    change translate header-bar

--- frontend/src/app/i18n.ts
+++ frontend/src/app/i18n.ts
@@ -1,8 +1,72 @@
 export const i18nAsset = {
-    'en-US': {
-        demo: "English"
+  // <h3>{{ 'home.demo' | translate }}</h3>
+
+  'en-US': {
+    // tên trang
+    header_bar: {
+      home: 'Home',
+      about: 'About',
+      academic: 'Academic',
+      examRoutine: 'Exam Routine',
+      classEBook: 'All Class E-Book',
+      result: 'Result',
+      tutionFee: 'Tution Fee',
+      contact: 'Contact',
+      library: 'Library',
+      // About
+      aboutUs: 'About Us',
+      ourPrincipal: 'Our Principal',
+      missionVision: 'Mission & Vision',
+      facultyMember: 'Faculty Member',
+      // Academic
+      classRoutine: 'Class Routine',
+      events: 'Academic Events',
+      calender: 'Academic Calender',
+      rulesRegulations: 'Rules & Regulations',
+      facultyInformations: 'Faculty Informations',
+      // Exam Routine - Result
+      halfExam: 'half-early exam',
+      tutorialExam: 'Tutorial Exam ',
+      finalExam: 'Final Exam',
+      scc: 'SSC Exam',
+      jsc: 'JSC Exam',
     },
-    'vn-VN': {
-        demo: "VietNam"
+    home: {
+      demo: 'English',
     },
+  },
+  'vn-VN': {
+    // tên trang
+    header_bar: {
+      home: 'Trang chủ',
+      about: 'Thông tin',
+      academic: 'Academic',
+      examRoutine: 'Quy trình kiểm tra',
+      classEBook: 'Các lớp E-Book',
+      result: 'Kết quả',
+      tutionFee: 'Học phí',
+      contact: 'Liên hệ',
+      library: 'Thư viện',
+      // About
+      aboutUs: 'Thông tin trường',
+      ourPrincipal: 'Thông tin hiệu trưởng',
+      missionVision: 'Sứ mệnh & Tầm nhìn',
+      facultyMember: 'Thành viên khoa',
+      // Academic
+      classRoutine: 'Quy trình lớp học',
+      events: 'Sự kiện học tập',
+      calender: 'Lịch học',
+      rulesRegulations: 'Nội quy & Quy định',
+      facultyInformations: 'Thông tin Khoa',
+      // Exam Routine - Result
+      halfExam: 'Thi giữa kỳ',
+      tutorialExam: 'Bài kiểm tra hướng dẫn',
+      finalExam: 'Thi cuối kỳ',
+      scc: 'Thi SSC',
+      jsc: 'Thi JSC',
+    },
+    home: {
+      demo: 'VietNam',
+    },
+  },
 };
