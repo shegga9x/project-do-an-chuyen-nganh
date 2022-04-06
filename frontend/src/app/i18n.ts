0f4df33d66bc9d77239517c97c706c72e0fc00d5
tranmanhtuong123 <18130275@st.hcmuaf.edu.vn>
2022-04-06 22:43:15 +0700

    change translate footer

--- frontend/src/app/i18n.ts
+++ frontend/src/app/i18n.ts
@@ -31,6 +31,29 @@ export const i18nAsset = {
       scc: 'SSC Exam',
       jsc: 'JSC Exam',
     },
+
+    footer: {
+      topLink: 'Top Link',
+      importantURL: 'Important URL',
+      // columm 1
+      committee: 'Committee',
+      manpower: 'Manpower',
+      admissionInformation: 'Admission information',
+      admissionForm: 'Admission Form',
+      classRoutine: 'Class routine',
+      attendance: 'Attendance',
+      // columm 2
+      board: 'Education Board',
+      boardResults: 'Education Board Results',
+      portal: 'Portal',
+      publicLibrary: 'Public Library',
+      ebook: 'Ebook',
+      // columm 3
+      managementManual: 'Online Student Management Manual',
+      scc: 'Name Correction SSC',
+      about: 'About Web Site And How to log-in',
+    },
+
     home: {
       demo: 'English',
     },
@@ -65,6 +88,29 @@ export const i18nAsset = {
       scc: 'Thi SSC',
       jsc: 'Thi JSC',
     },
+
+    footer: {
+      topLink: 'Liên kết hàng đầu',
+      importantURL: 'Liên kết quan trọng',
+      // columm 1
+      committee: 'Ủy ban',
+      manpower: 'Nhân lực',
+      admissionInformation: 'Thông tin tuyển sinh',
+      admissionForm: 'Hình thức tuyển sinh',
+      classRoutine: 'Lịch trình lớp học',
+      attendance: 'Sự tham dự',
+      // columm 2
+      board: 'Hội đồng Giáo dục',
+      boardResults: 'Kết quả của Hội đồng Giáo dục',
+      portal: 'Cổng thông tin',
+      publicLibrary: 'Thư viện công cộng',
+      ebook: 'Sách điện tử',
+      // columm 3
+      managementManual: 'Sổ tay Quản lý Sinh viên Trực tuyến',
+      scc: 'SSC',
+      about: 'Giới thiệu về trang web và cách đăng nhập',
+    },
+
     home: {
       demo: 'VietNam',
     },
