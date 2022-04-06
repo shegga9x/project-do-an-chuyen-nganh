eb7609785755457d75fbeda2688e14c68e08099e
minhhien <18130076@st.hcmuaf.edu.vn>
2022-04-06 12:42:15 +0700

    add capcha

--- frontend/src/app/components/user/shared/dialog-login/dialog-login.component.ts
+++ frontend/src/app/components/user/shared/dialog-login/dialog-login.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, ChangeDetectorRef } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { GeneralService } from 'src/app/services/general.service';
 import { first } from 'rxjs/operators';
@@ -17,8 +17,22 @@ export class DialogLoginComponent implements OnInit {
   form: FormGroup;
   submitted = false;
 
+  // capcha siteKey
+  public readonly siteKey = '6LcvoUgUAAAAAJJbhcXvLn3KgG-pyULLusaU4mL1';
+
+  // capcha variable
+  public theme: 'light' | 'dark' = 'light';
+  public size: 'compact' | 'normal' = 'normal';
+  public lang = 'en';
+  public type: 'image' | 'audio';
+
+  public captchaIsLoaded = false;
+  public captchaSuccess = false;
+  public captchaIsExpired = false;
+  public captchaResponse?: string;
+
   constructor(private formBuilder: FormBuilder,
-    private route: ActivatedRoute,
+    private cdr: ChangeDetectorRef,
     private router: Router,
     private accountService: AccountService,
     private generealService: GeneralService,
@@ -27,6 +41,7 @@ export class DialogLoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
+      recaptcha: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
     });
@@ -35,6 +50,34 @@ export class DialogLoginComponent implements OnInit {
   // convenience getter for easy access to form fields
   get f() { return this.form.controls; }
 
+
+  //capcha function
+
+  handleReset(): void {
+    this.captchaSuccess = false;
+    this.captchaResponse = undefined;
+    this.captchaIsExpired = false;
+    this.cdr.detectChanges();
+  }
+
+  handleExpire(): void {
+    this.captchaSuccess = false;
+    this.captchaIsExpired = true;
+    this.cdr.detectChanges();
+  }
+
+  handleLoad(): void {
+    this.captchaIsLoaded = true;
+    this.captchaIsExpired = false;
+    this.cdr.detectChanges();
+  }
+
+  handleSuccess(captchaResponse: string): void {
+    this.captchaSuccess = true;
+    this.captchaResponse = captchaResponse;
+    this.captchaIsExpired = false;
+    this.cdr.detectChanges();
+  }
 
   onSubmit() {
     this.submitted = true;
