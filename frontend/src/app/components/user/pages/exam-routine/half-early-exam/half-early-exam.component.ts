fed3759bfb152964c1b8a4308034ba2021b86e77
DK2801 <khanh2812000123@gmail.com>
2022-04-12 22:30:29 +0700

    finish_xemDSSV

--- frontend/src/app/components/user/pages/exam-routine/half-early-exam/half-early-exam.component.ts
+++ frontend/src/app/components/user/pages/exam-routine/half-early-exam/half-early-exam.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from "@angular/platform-browser";
+import { ActivatedRoute } from '@angular/router';
+import { CourseManageService } from 'src/app/services';
 
 @Component({
   selector: 'app-half-early-exam',
@@ -7,12 +9,42 @@ import { Title } from "@angular/platform-browser";
   styleUrls: ['./half-early-exam.component.scss']
 })
 export class HalfEarlyExamComponent implements OnInit {
-
-  constructor(private titleService: Title) {
+  listStudent: any[] = []
+  listStudentAfterSort: any[] = []
+  constructor(private titleService: Title, private route: ActivatedRoute, private courseManageService: CourseManageService) {
     this.titleService.setTitle("Half Early Exam");
   }
 
   ngOnInit(): void {
-  }
+    this.route.queryParams.subscribe(params => {
+      this.getListStudentBySubject(params.idSCHEDULE)
 
+
+    });
+  }
+  getListStudentBySubject(idSCHEDULE: any) {
+    this.courseManageService
+      .getListStudentBySubjectRegist(idSCHEDULE)
+      .subscribe({
+        next: (x: any) => {
+          this.listStudent = x
+          this.listStudentAfterSort = x;
+          console.log(x)
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
+  }
+  handleChange(event: any, field: string) {
+    this.listStudentAfterSort = this.listStudent.sort((a, b) => {
+        if (a[field] < b[field]) {
+          return -1;
+        }
+        if (a[field] > b[field]) {
+          return 1;
+        }
+        return 0;
+      });
+  }
 }
