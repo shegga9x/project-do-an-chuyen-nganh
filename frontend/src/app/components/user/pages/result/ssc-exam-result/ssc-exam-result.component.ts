948339d0b9c2450c564e8fcecd226335b13c211a
minhhien <18130076@st.hcmuaf.edu.vn>
2022-04-21 18:33:48 +0700

    merg

--- frontend/src/app/components/user/pages/result/ssc-exam-result/ssc-exam-result.component.ts
+++ frontend/src/app/components/user/pages/result/ssc-exam-result/ssc-exam-result.component.ts
@@ -33,23 +33,42 @@ export class SscExamResultComponent implements OnInit {
       let request = {
         idCourseOffering: "52", idSemester: "2021_2", is4Max: false, subScoreModels: this.listFull
       }
-      this.pDTService.addScoreFromExcel(request).then(x2 => {
-        this.finish = true;
-      }).catch((err) => {
-        console.log(err)
-        let objectError = JSON.parse(err)
-        this.listError = this.listFull.filter(obj => {
-          return objectError[obj['studentID']] != undefined
-        })
-        this.listError.forEach(element => {
-          element.mess = objectError[element['studentID']]
-        });
-        this.listOk = this.listFull.filter(obj => {
-          return objectError[obj['studentID']] == undefined
-        })
-        this.finish = true;
-        console.log(this.listFull)
-      });
+      //   this.pDTService.addScoreFromExcel(request).subscribe(x2 => {
+      //     this.finish = true;
+      //   }).catch((err) => {
+      //     console.log(err)
+      //     let objectError = JSON.parse(err)
+      //     this.listError = this.listFull.filter(obj => {
+      //       return objectError[obj['studentID']] != undefined
+      //     })
+      //     this.listError.forEach(element => {
+      //       element.mess = objectError[element['studentID']]
+      //     });
+      //     this.listOk = this.listFull.filter(obj => {
+      //       return objectError[obj['studentID']] == undefined
+      //     })
+      //     this.finish = true;
+      //     console.log(this.listFull)
+      //   });
+      // })
+      this.pDTService.addAccountFromExcel(request).subscribe({
+        next: (x2) => {},
+        error: (err) => {
+          console.log(err)
+          let objectError = JSON.parse(err)
+          this.listError = this.listFull.filter(obj => {
+            return objectError[obj['studentID']] != undefined
+          })
+          this.listError.forEach(element => {
+            element.mess = objectError[element['studentID']]
+          });
+          this.listOk = this.listFull.filter(obj => {
+            return objectError[obj['studentID']] == undefined
+          })
+          this.finish = true;
+          console.log(this.listFull)
+        }
+      })
     })
   }
 }
