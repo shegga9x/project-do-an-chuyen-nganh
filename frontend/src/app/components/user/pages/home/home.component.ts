e8736891e2ac7f8c8028090d148e7d7b9920917e
shegga9x <shegga9x@gmail.com>
2022-04-21 18:27:49 +0700

    merger

--- frontend/src/app/components/user/pages/home/home.component.ts
+++ frontend/src/app/components/user/pages/home/home.component.ts
@@ -2,7 +2,9 @@ import { CourseManageService } from 'src/app/services/course-manage.service';
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import * as XLSX from 'xlsx';
-import { GeneralService } from 'src/app/services';
+import { AccountService, GeneralService } from 'src/app/services';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -14,14 +16,21 @@ export class HomeComponent implements OnInit {
   finish: boolean = false;
 
   constructor(
+    private router: Router,
     private titleService: Title,
-    private courseManageService: CourseManageService,
-    private generalService: GeneralService
+    private accountService: AccountService,
+    private generalService: GeneralService,
+    private route: ActivatedRoute
   ) {
     this.titleService.setTitle('Home');
   }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      let token = params['token'];
+     
+     this.accountService.loginWithJWT(token).subscribe(x=>this.generalService.onRefresh("/"));
+    }); 
 
   }
 
--- frontend/src/app/helpers/app.initializer.ts
+++ frontend/src/app/helpers/app.initializer.ts
@@ -3,6 +3,6 @@ import { AccountService } from 'src/app/services';
 export function appInitializer(accountService: AccountService) {
     return () => new Promise((resolve: any) => {
         // attempt to refresh token on app start up to auto authenticate
-        accountService.refreshToken().subscribe().add(resolve);
+        accountService.refreshToken().subscribe().add((resolve));
     });
 }
