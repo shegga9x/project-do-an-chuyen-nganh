1da6ae844613c4b88e1493ec5c8fe20b134f084a
tranmanhtuong123 <18130275@st.hcmuaf.edu.vn>
2022-06-08 16:17:57 +0700

    update i18n

--- frontend/src/app/i18n.ts
+++ frontend/src/app/i18n.ts
@@ -5,32 +5,32 @@ export const i18nAsset = {
     // tên trang
     header_bar: {
       home: 'Home',
+
+      courseRegistration: 'Course registration',
+      timeTable: 'Time table',
+      reportGrade: 'Report Grade',
+      reportDateExam: 'Report Date Exam',
+      reportSchoolFees: 'Report School Fees',
+      courseProgram: 'Course Program',
+
       about: 'About',
-      academic: 'Academic',
-      examRoutine: 'Exam Routine',
-      classEBook: 'All Class E-Book',
-      result: 'Result',
-      gallery:'GALLERY',
-      tutionFee: 'Tution Fee',
       contact: 'Contact',
-      library: 'Library',
       // About
       aboutUs: 'About Us',
       ourPrincipal: 'Our Principal',
       missionVision: 'Mission & Vision',
       facultyMember: 'Faculty Member',
-      // Academic
-      classRoutine: 'Class Routine',
-      events: 'Academic Events',
-      calender: 'Academic Calender',
-      rulesRegulations: 'Rules & Regulations',
-      facultyInformations: 'Faculty Informations',
-      // Exam Routine - Result
-      halfExam: 'half-early exam',
-      tutorialExam: 'Tutorial Exam ',
-      finalExam: 'Final Exam',
-      scc: 'SSC Exam',
-      jsc: 'JSC Exam',
+      // CourseRegistration
+
+      // Time Table
+
+      // Report Grade
+
+      // Report Date Exam
+
+      // Report School Fees
+
+      // Course Program
     },
 
     footer: {
@@ -64,12 +64,14 @@ export const i18nAsset = {
     header_bar: {
       home: 'Trang chủ',
       about: 'Thông tin',
-      academic: 'Academic',
-      examRoutine: 'Quy trình kiểm tra',
-      classEBook: 'Các lớp E-Book',
-      result: 'Kết quả',
-      gallery:'Bộ sưu tập',
-      tutionFee: 'Học phí',
+
+      courseRegistration: 'Đăng ký môn học',
+      timeTable: 'Thời khóa biểu',
+      reportGrade: 'Xem điểm',
+      reportDateExam: 'Xem lịch thi',
+      reportSchoolFees: 'Xem học phí',
+      courseProgram: 'Xem CTĐT',
+
       contact: 'Liên hệ',
       library: 'Thư viện',
       // About
@@ -77,18 +79,11 @@ export const i18nAsset = {
       ourPrincipal: 'Thông tin hiệu trưởng',
       missionVision: 'Sứ mệnh & Tầm nhìn',
       facultyMember: 'Thành viên khoa',
-      // Academic
-      classRoutine: 'Quy trình lớp học',
-      events: 'Sự kiện học tập',
-      calender: 'Lịch học',
-      rulesRegulations: 'Nội quy & Quy định',
-      facultyInformations: 'Thông tin Khoa',
-      // Exam Routine - Result
-      halfExam: 'Thi giữa kỳ',
-      tutorialExam: 'Bài kiểm tra hướng dẫn',
-      finalExam: 'Thi cuối kỳ',
-      scc: 'Thi SSC',
-      jsc: 'Thi JSC',
+      // CourseRegistration
+
+      // Time Table
+
+      //
     },
 
     footer: {
